93d54587fb409602a36a2ff525ef04ecafba04ef
pednucci <pedropetenucci@gmail.com>
2021-04-21 19:55:47 -0300

    day3: user repository method added

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,13 +1,20 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
 import { UserRepository } from "../repositories/UsersRepository";
 
 
 class UsersService {
-    async create(email: string) {
-        const usersRepository = getCustomRepository(UserRepository)
-        //Verificar se o usuário existe
+    private usersRepository: Repository<User>
 
-        const userExists = await usersRepository.findOne({
+    constructor() {
+        this.usersRepository = getCustomRepository(UserRepository);
+    }
+
+    async create(email: string) {
+
+
+        //Verificar se o usuário existe
+        const userExists = await this.usersRepository.findOne({
             email
         })
 
@@ -19,11 +26,11 @@ class UsersService {
 
         //Se não existir, salvar no DB
         
-        const user = usersRepository.create({
+        const user = this.usersRepository.create({
             email,
         });
 
-        await usersRepository.save(user);
+        await this.usersRepository.save(user);
 
         return user;
     }

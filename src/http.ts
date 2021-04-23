3b9d979d904272a012113068d71942b2c373389a
pednucci <pedropetenucci@gmail.com>
2021-04-22 22:44:31 -0300

    day4: separate http protocol

---
+++ src/http.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from 'path'; //leitura de caminhos de arquivo
+
+import './database';
+import { routes } from './routes'
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require("ejs").renderFile);
+app.set('view engine', 'html');
+
+app.get("/pages/client", (req, res) => {
+    res.render('html/client.html');
+})
+
+const http = createServer(app) // criando protocolo http
+const io = new Server(http) // criando protocolo ws
+
+io.on("connection", (socket: Socket) => {
+    // console.log("Socket conectado: " + socket.id)
+})
+
+app.use(express.json());
+
+app.use(routes);
+
+export { http, io }
